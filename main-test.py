1ad17c294841c54e1baf7849fb9d0fc756c836e9
DevTester <devtester3490@outlook.com>
2021-11-20 18:11:48 -0500

    fix tabs

--- main-test.py
+++ main-test.py
@@ -2,10 +2,9 @@
 from main import Add
 
 def TestAdd():
-        assert Add(2,3) == 5
-        assert Add(5,3) == 20
-
-        print("Add Function works correctly")
+	assert Add(2,3) == 5
+	assert Add(5,3) == 20
+	print("Add Function works correctly")
 
 if __name__ == '__main__':
         TestAdd()
